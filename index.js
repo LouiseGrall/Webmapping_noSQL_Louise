e6a2091e3a97b2727d31f3fd5f913b77ce308187
LouiseGrall <louisegrall@yahoo.fr>
2021-03-18 16:12:46 +0100

    MAJ1

--- index.js
+++ index.js
@@ -7,33 +7,75 @@ var multer = require('multer');
 var upload = multer();
 var filter = {};
 
-
-app.get('/geo-search-results', function(req, res){
- //console.log(req.query);
-
-var latitude = parseFloat(req.query.latitude);
-var longitude = parseFloat(req.query.longitude);
-var radius = parseFloat(req.query.radius);
-
-if (Math.abs(longitude) > 0.00001 &&
-     Math.abs(latitude) > 0.00001) {
-
-filter.geometry = { "$geoWithin": { "$center": [ [ longitude, latitude ] , radius ] } };
- }
- // console.log("filter", filter, [ longitude, latitude ]);
-console.log("toto",mydb.collection,mydb);
-
-mydb.collection('equip').find(filter).toArray(function(err, docs) {
-   console.log("Found "+docs.length+" records");
-   // console.dir(docs);
-   res.render('geo-search-results', {
-     results: docs
-   });
- });
+MongoClient.connect("mongodb://localhost:27017/geo", function(err, db) {
+  if (err){
+    console.log("dberror",err);
+  }
+  else{
+    console.log("Connected correctly to server");
+    mydb = db;
+    app.listen(3000);
+    console.log("Server listening...");
+  }
 });
 
+app.get('/geo-search-results', function(req, res){
+  //console.log(req.query);
+
+  var latitude = parseFloat(req.query.latitude);
+  var longitude = parseFloat(req.query.longitude);
+  var radius = parseFloat(req.query.radius);
+
+  if (Math.abs(longitude) > 0.00001 &&
+  Math.abs(latitude) > 0.00001) {
+    filter.geometry = { "$geoWithin": { "$center": [ [ longitude, latitude ] , radius ] } };
+  }
+
+  // console.log("filter", filter, [ longitude, latitude ]);
+  //console.log("toto",mydb);
+
+
+  mydb.collection('equip').find(filter).toArray(function(err, docs) {
+    console.log("Found "+docs.length+" records");
+    // console.dir(docs);
+    res.render('geo-search-results', {
+      results: docs
+    });
+  });
+});
+
+app.get('/geo-search-results-json', function(req, res){
+  //console.log(req.query);
+
+  var latitude = parseFloat(req.query.latitude);
+  var longitude = parseFloat(req.query.longitude);
+  var radius = parseFloat(req.query.radius);
+
+  if (Math.abs(longitude) > 0.00001 &&
+  Math.abs(latitude) > 0.00001) {
+    filter.geometry = { "$geoWithin": { "$center": [ [ longitude, latitude ] , radius ] } };
+  }
+
+  // console.log("filter", filter, [ longitude, latitude ]);
+  //console.log("toto",mydb);
+
+
+  mydb.collection('equip').find(filter).toArray(function(err, docs) {
+    console.log("Found "+docs.length+" records");
+    // console.dir(docs);
+    res.json(
+      {
+        "type": "FeatureCollection",
+        "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+        "features": docs
+      }
+    );
+  });
+});
+
+
 app.get('/geo-search', function(req, res){
-   res.render('form');
+  res.render('form');
 });
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -48,13 +90,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 app.use(express.static('public'));
 app.post('/', function(req, res){
-   console.log(req.body);
-   res.send("received your request!");
+  console.log(req.body);
+  res.send("received your request!");
 });
-
-MongoClient.connect("mongodb://localhost:27017/geo", function(err, db) {
- console.log("Connected correctly to server");
- mydb = db;
- app.listen(3000);
- console.log("Server listening...");
-});
+app.use('/static', express.static('public'));
